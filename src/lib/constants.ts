04f9f3a85d1abebfc6c740c5a7c1d8aa82028f7e
Hicham DIAB <diabhicham987@gmail.com>
2024-10-12 00:30:23 +0100

    feat: - init project with shadcn - setup dark theme - setup multi lang with next-intl

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const languages = [
+  { label: 'Français', value: 'fr' },
+  { label: 'English', value: 'en' },
+  { label: 'Español', value: 'es' },
+  { label: 'العربية', value: 'ar' },
+]
+
+// lang array value
+export const languagesValues = languages.map((lang) => lang.value) 
