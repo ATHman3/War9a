f2c27f3e0b6b929747876af8bfa3496bb0e835f3
Hicham DIAB <diabhicham987@gmail.com>
2024-10-12 01:37:32 +0100

    feat: add smooth transition for theme changes and improve ThemeProvider mounting

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,10 +1,19 @@
-'use client'
-import { ThemeProvider } from 'next-themes'
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function ThemeProviders({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  // Only allow the theme switcher to appear once the component is mounted
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true}>
       {children}
     </ThemeProvider>
-  )
+  );
 }
